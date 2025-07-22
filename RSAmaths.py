b275fef1cad7b045b14e288fd30a1f13bc178cbb
Steve Marchant <44552903+mcl-uk@users.noreply.github.com>
2025-07-22 17:39:16 +0100

    Update RSAmaths.py

--- RSAmaths.py
+++ RSAmaths.py
@@ -40,7 +40,7 @@ assert u %e != 0  # - this is just one of the rules of RSA.
 # be calculated using the 'extended Euclidian algorithm' or more conveniently,
 # in modern python implementations, we can just use the pow() function thusly:
 d = pow(e, -1, u)
-assert e*d %u == 1
+assert (e*d) %u == 1
 # since this is such an important step let's get a better handle on it
 # with some actual numbers so you can see how it plays out:
 print(f'Check that e*d %u == 1: {e}*{d} = {e*d}, %{u} = {e*d %u} -- YES\n')
