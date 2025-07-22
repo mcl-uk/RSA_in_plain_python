361a836036b592aa3e94f18d5170782b4c1ac4fe
Steve Marchant <44552903+mcl-uk@users.noreply.github.com>
2025-07-22 17:35:02 +0100

    Update RSAmaths.py

--- RSAmaths.py
+++ RSAmaths.py
@@ -9,7 +9,7 @@
 # the message knowing only the public key. It was the RSA algorithm that first made
 # this amazing feat possible. And it's still in use today at the bedrock of every-day
 # internet security and authentication.  Note that RSA is only suited to encrypting
-# relatively short messages and in fact is used mostly for key exchange to enable
+# relatively short messages and in fact is used mostly for key distribution to enable
 # subsequent symetrically-encrypted communication eg using AES.
 # Here I present a detailed, python-based, mathematical explanation of how & why the
 # RSA algoritm works, illustrated by a simple key-generation/encrypt/decrypt example.
